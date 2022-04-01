fcdffa536ebda322aea8ebb5c7d8c3b30a378e70
Gilbert Ngeno <ngenogilbert07@gmail.com>
2022-04-01 22:45:00 +0300

    UserDetailsService
    UserDetails implementation
    
    Supporting beans - PasswordEncoder,TokenStore

--- src/main/java/com/example/copsboot/user/User.java
+++ src/main/java/com/example/copsboot/user/User.java
@@ -1,24 +1,28 @@
 package com.example.copsboot.user;
 
 import com.example.orm.jpa.AbstractEntity;
+import com.google.common.collect.Sets;
 import com.sun.istack.NotNull;
 
 import javax.persistence.*;
+
 import java.util.Set;
+
 
 @Entity
 @Table(name = "copsboot_user")
 public class User extends AbstractEntity<UserId> {
-    @Id
-    private UserId id;
+
     private String email;
     private String password;
+
     @ElementCollection(fetch = FetchType.EAGER)
     @Enumerated(EnumType.STRING)
     @NotNull
     private Set<UserRole> roles;
 
     protected User() {
+
     }
 
     public User(UserId id, String email, String password, Set<UserRole> roles) {
@@ -26,5 +30,25 @@ public class User extends AbstractEntity<UserId> {
         this.email = email;
         this.password = password;
         this.roles = roles;
+    }
+
+    public static User createOfficer(UserId userId, String email, String encodedPassword) {
+        return new User(userId, email, encodedPassword, Sets.newHashSet(UserRole.OFFICER));
+    }
+
+    public static User createCaptain(UserId userId, String email, String encodedPassword) {
+        return new User(userId, email, encodedPassword, Sets.newHashSet(UserRole.CAPTAIN));
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
     }
 }
---
+++ src/main/java/com/example/copsboot/user/UserService.java
@@ -0,0 +1,4 @@
+package com.example.copsboot.user;
+public interface UserService {
+    User createOfficer(String email, String password);
+}
